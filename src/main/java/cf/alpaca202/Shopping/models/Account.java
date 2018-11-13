d9cd0cc0ae4396ae3602a7783b9557f76548ffb1
boat46450 <boat46450@hotmail.com>
2018-11-13 09:23:06 +0700

    create account model

---
+++ src/main/java/cf/alpaca202/Shopping/models/Account.java
@@ -0,0 +1,166 @@
+package cf.alpaca202.Shopping.models;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "accounts")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Account implements Serializable {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  private String email;
+  private String password;
+  private String firstname;
+  private String lastname;
+  private Date birthday;
+
+  @OneToMany(cascade = CascadeType.ALL,
+          fetch = FetchType.LAZY,
+          mappedBy = "address")
+  private List<Address> addresses;
+
+  @Column(name = "image_path")
+  private String imagePath;
+
+  @Column(name = "phone_number")
+  private String phoneNumber;
+
+  @Temporal(TemporalType.TIMESTAMP)
+  @CreatedDate
+  @Column(name = "created_at")
+  private Date createdAt;
+
+  @Temporal(TemporalType.TIMESTAMP)
+  @LastModifiedDate
+  @Column(name = "updated_at")
+  private Date updatedAt;
+
+  public Account() {
+  }
+
+  public Account(long id, String email, String password, String firstname, String lastname, Date birthday, List<Address> addresses, String imagePath, String phoneNumber, Date createdAt, Date updatedAt) {
+    this.id = id;
+    this.email = email;
+    this.password = password;
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.birthday = birthday;
+    this.addresses = addresses;
+    this.imagePath = imagePath;
+    this.phoneNumber = phoneNumber;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+
+  public long getId() {
+    return this.id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getEmail() {
+    return this.email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return this.password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getFirstname() {
+    return this.firstname;
+  }
+
+  public void setFirstname(String firstname) {
+    this.firstname = firstname;
+  }
+
+  public String getLastname() {
+    return this.lastname;
+  }
+
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
+
+  public Date getBirthday() {
+    return this.birthday;
+  }
+
+  public void setBirthday(Date birthday) {
+    this.birthday = birthday;
+  }
+
+  public List<Address> getAddresses() {
+    return this.addresses;
+  }
+
+  public void setAddresses(List<Address> addresses) {
+    this.addresses = addresses;
+  }
+
+  public String getImagePath() {
+    return this.imagePath;
+  }
+
+  public void setImagePath(String imagePath) {
+    this.imagePath = imagePath;
+  }
+
+  public String getPhoneNumber() {
+    return this.phoneNumber;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  public Date getCreatedAt() {
+    return this.createdAt;
+  }
+
+  public void setCreatedAt(Date createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public Date getUpdatedAt() {
+    return this.updatedAt;
+  }
+
+  public void setUpdatedAt(Date updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+}
