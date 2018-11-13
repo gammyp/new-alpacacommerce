2bb10e1f9e19a695fd38aeb8f1e99352c53c78b2
boat46450 <boat46450@hotmail.com>
2018-11-13 10:52:56 +0700

    enable auto config

--- src/main/java/cf/alpaca202/Shopping/ShoppingApplication.java
+++ src/main/java/cf/alpaca202/Shopping/ShoppingApplication.java
@@ -1,10 +1,12 @@
 package cf.alpaca202.Shopping;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
+@EnableAutoConfiguration
 @SpringBootApplication
 public class ShoppingApplication {
 
