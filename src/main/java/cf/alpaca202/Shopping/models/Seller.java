50ccbfae8686a57e00e69fe3eaeda99c5f3dc032
boat46450 <boat46450@hotmail.com>
2018-11-14 18:14:46 +0700

    change relation between account and seller from one to many to one to one

--- src/main/java/cf/alpaca202/Shopping/models/Seller.java
+++ src/main/java/cf/alpaca202/Shopping/models/Seller.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -37,7 +38,7 @@ public class Seller implements Serializable {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
 
-  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  @OneToOne(fetch = FetchType.LAZY, optional = false)
   @JoinColumn(name="account_id")
   @OnDelete(action = OnDeleteAction.CASCADE)
   @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
