3a4a1d5d728cbd648b8836bd3721c4adcfd7660f
TanawatRyu <gunmag3307985@hotmail.com>
2018-11-14 11:42:57 +0700

    [EDIT] master : method getAllProducts() get data from ProductService

--- src/main/java/cf/alpaca202/Shopping/controllers/ProductController.java
+++ src/main/java/cf/alpaca202/Shopping/controllers/ProductController.java
@@ -1,13 +1,26 @@
 package cf.alpaca202.Shopping.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import cf.alpaca202.Shopping.models.Product;
+import cf.alpaca202.Shopping.services.ProductService;
+
 @Controller
 public class ProductController {
+  
+  @Autowired
+  private ProductService productService;
+  
   @RequestMapping(name = "/", method = RequestMethod.GET)
-  public String getAllProducts() {
+  public String getAllProducts(Model model) {
+    List<Product> listProduct = productService.getAllProducts();
+    model.addAttribute("product",listProduct);
     return "index";
   }
 }
