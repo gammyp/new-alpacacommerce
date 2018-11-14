775f3c22f249e1e225dab6eda3abdfa54c943428
TanawatRyu <gunmag3307985@hotmail.com>
2018-11-14 11:19:32 +0700

    [EDIT] master : add list findByProductId

--- src/main/java/cf/alpaca202/Shopping/repository/ProductVarientRepository.java
+++ src/main/java/cf/alpaca202/Shopping/repository/ProductVarientRepository.java
@@ -1,5 +1,7 @@
 package cf.alpaca202.Shopping.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import cf.alpaca202.Shopping.models.ProductVarient;
 
 @Repository
 public interface ProductVarientRepository extends JpaRepository<ProductVarient, Long>{
+    
+    List<ProductVarient> findByProductId(Long productId);
 
 }
