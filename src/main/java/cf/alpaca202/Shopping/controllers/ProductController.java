371b177a2b8ee3e0b897c8b6887c7bec800d295e
TanawatRyu <gunmag3307985@hotmail.com>
2018-11-14 12:26:00 +0700

    [EDIT] master : add method getProduct() in ProductController

--- src/main/java/cf/alpaca202/Shopping/controllers/ProductController.java
+++ src/main/java/cf/alpaca202/Shopping/controllers/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -19,8 +20,15 @@ public class ProductController {
   
   @RequestMapping(name = "/", method = RequestMethod.GET)
   public String getAllProducts(Model model) {
-    List<Product> listProduct = productService.getAllProducts();
-    model.addAttribute("product",listProduct);
+    List<Product> productList = productService.getAllProducts();
+    model.addAttribute("productlist",productList);
     return "index";
+  }
+
+  @RequestMapping(name = "/product/{product_id}", method = RequestMethod.GET)
+  public String getProduct(Model model, @PathVariable(name = "product_id") Long productId) {
+    Product product = productService.getProductById(productId);
+    model.addAttribute("product",product);
+    return "productdetail";
   }
 }
