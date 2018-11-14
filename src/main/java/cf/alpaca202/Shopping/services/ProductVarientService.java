c07ec4c3584b04ee9d4b0a051f251a6fd0644e02
TanawatRyu <gunmag3307985@hotmail.com>
2018-11-14 11:19:57 +0700

    [ADD] master : create ProductVarientService with method getProductVarientByProductId()

---
+++ src/main/java/cf/alpaca202/Shopping/services/ProductVarientService.java
@@ -0,0 +1,20 @@
+package cf.alpaca202.Shopping.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cf.alpaca202.Shopping.models.ProductVarient;
+import cf.alpaca202.Shopping.repository.ProductVarientRepository;
+
+@Service
+public class ProductVarientService{
+
+    @Autowired
+    private ProductVarientRepository productVarientRepository;
+
+    public List<ProductVarient> getProductVarientByProductId(Long productId){
+        return productVarientRepository.findByProductId(productId);
+    }
+}
