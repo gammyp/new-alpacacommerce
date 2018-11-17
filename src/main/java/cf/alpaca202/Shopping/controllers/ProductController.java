e5b431ad25ca637b761348d691f10c23ab7ecec2
boat46450 <boat46450@hotmail.com>
2018-11-17 10:54:20 +0700

    fix logic

--- src/main/java/cf/alpaca202/Shopping/controllers/ProductController.java
+++ src/main/java/cf/alpaca202/Shopping/controllers/ProductController.java
@@ -10,25 +10,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import cf.alpaca202.Shopping.models.Product;
+import cf.alpaca202.Shopping.models.ProductVarient;
 import cf.alpaca202.Shopping.services.ProductService;
+import cf.alpaca202.Shopping.services.ProductVarientService;
 
 @Controller
 public class ProductController {
   
   @Autowired
   private ProductService productService;
+
+  @Autowired
+  private ProductVarientService productVarientService;
   
   @RequestMapping(value = "/", method = RequestMethod.GET)
   public String getAllProducts(Model model) {
-    List<Product> productList = productService.getAllProducts();
-    model.addAttribute("productlist",productList);
+    List<Product> products = productService.getAllProducts();
+    model.addAttribute("products", products);
     return "index";
   }
 
-  @RequestMapping(value = "/product/{product_id}", method = RequestMethod.GET)
-  public String getProduct(Model model, @PathVariable(name = "product_id") Long productId) {
+  @RequestMapping(value = "/product/{product_id}/{product_varient_id}", method = RequestMethod.GET)
+  public String getProduct(
+    Model model,
+    @PathVariable(name = "product_id") Long productId,
+    @PathVariable(name = "product_varient_id") Long productVarientId
+  ) {
     Product product = productService.getProductById(productId);
-    model.addAttribute("product",product);
+    ProductVarient productVarient = productVarientService.getProductVarientById(productVarientId);
+    model.addAttribute("product", product);
+    model.addAttribute("productVarient", productVarient);
     return "productdetail";
   }
 }
