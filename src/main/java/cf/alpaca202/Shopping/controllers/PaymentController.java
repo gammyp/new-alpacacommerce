4b7bc614fb39d40ff017feb12102ee98c0c451de
Jamiiz <pornvarat.sit22@mail.kmutt.ac.th>
2018-11-18 17:07:56 +0700

    Add type Controller

--- src/main/java/cf/alpaca202/Shopping/controllers/PaymentController.java
+++ src/main/java/cf/alpaca202/Shopping/controllers/PaymentController.java
@@ -1,14 +1,15 @@
 package cf.alpaca202.Shopping.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
-
+@Controller
 public class PaymentController {
-
+    
     @RequestMapping(value = "/payment", method = RequestMethod.GET)
     public String goToPay() {
         return "payment";
     }
+
 }
